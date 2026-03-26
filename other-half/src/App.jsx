82be1e9796f57393391bd07fca47a6a6ad134e43
mukeemansari69 <mukeemansari038@gmail.com>
2026-03-26 17:27:21 +0530

    complete product page

--- other-half/src/App.jsx
+++ other-half/src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import Home from './Home.jsx'
 import Headers from './Home/Header.jsx'
 import ProductBanner from './ProductBanner.jsx'
+import Marque from './Home/Marque.jsx'
+import ScoopOne from './ScoopOne.jsx'
+import Features from './Home/Features.jsx'
+import NotAll from './NotAll.jsx'
+import TrustedSection from './Home/TrustedSection.jsx'
+import QualitySection from './Home/QualitySection.jsx'
+import Footer from './Home/Footer.jsx'
 
 
 
@@ -11,7 +18,13 @@ const App = () => {
   {/*  <Home></Home> */}
   <Headers></Headers>
   <ProductBanner></ProductBanner>
-  
+  <Marque></Marque>
+  <ScoopOne></ScoopOne>
+  <Features></Features>
+  <NotAll></NotAll>
+  <TrustedSection></TrustedSection>
+  <QualitySection></QualitySection>
+  <Footer></Footer>
    </>
   )
 }
